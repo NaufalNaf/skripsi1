9f0d9fb289446b4cc134aa2a220d818ea4bcd7a3
NaufalNaf <naufalagungr235@gmail.com>
2024-07-02 14:38:32 +0700

    skripsi

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load model
+model = joblib.load('klasifikasi_obesitas.pkl')
+
+st.title('Obesity Level Classification')
+
+# Input pengguna
+height = st.number_input('Height (in meters)', min_value=0.5, max_value=2.5, value=1.75)
+weight = st.number_input('Weight (in kg)', min_value=20, max_value=200, value=70)
+gender = st.selectbox('Gender', ('Female', 'Male'))
+
+# Konversi gender ke bentuk numerik
+gender_num = 0 if gender == 'Female' else 1
+
+# Prediksi
+input_data = np.array([[height, weight, age, gender_num]])
+prediction = model.predict(input_data)
+
+# Tampilkan hasil
+obesity_level = ['Extremely Weak','weak','Normal', 'Overweight', 'Obese','Extremely Obese']
+st.write(f'The predicted obesity level is: {obesity_level[prediction[0]]}')
